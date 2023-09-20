bc5ca7e3de0d2d927f8c5c5546d504bc202acaff
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-09-19 22:56:22 -0400

    Use python for CO687, I guess

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
+use std::collections::HashMap;
 use std::fs;
-use waterloo_crypto::vigenere;
+
+fn rank_frequencies(ciphertext_str: &str) -> Vec<f64> {
+    let mut counts: HashMap<char, usize> = HashMap::new();
+    let mut frequencies: Vec<f64> = vec![];
+
+    ciphertext_str.chars().for_each(|char_| {
+        let count = counts.get(&char_).unwrap_or(&0);
+        counts.insert(char_, *count + 1);
+    });
+
+    counts.iter().for_each(|(_, count)| {
+        let frequency = (*count as f64) / (ciphertext_str.len() as f64);
+        frequencies.push(frequency);
+    });
+    frequencies.sort_by(|f1, f2| f2.partial_cmp(f1).unwrap());
+    while frequencies.len() < 26 {
+        frequencies.push(0.0);
+    }
+
+    return frequencies;
+}
 
 fn main() {
-    let text = fs::read_to_string("inputs/q1-mobydick.txt").unwrap();
-    let reference_ioc = vigenere::index_of_coincidence(&text);
-    println!("{}", reference_ioc * 26.);
+    let ciphertexts_frequencies = (0..4)
+        .map(|i| {
+            let path = format!("inputs/a1q2ciphertexts/ctxt{}.txt", i);
+            let ciphertext_str = fs::read_to_string(path).unwrap();
+            let frequencies = rank_frequencies(&ciphertext_str);
+            return frequencies;
+        })
+        .collect::<Vec<Vec<f64>>>();
 
-    let ciphertext = fs::read_to_string("inputs/q1-ciphertext.txt").unwrap();
-    let blocksizes =
-        vigenere::search_blocksize(ciphertext.as_bytes(), reference_ioc, 100);
-    blocksizes.iter().for_each(|(blocksize, score)| {
-        println!("{blocksize}: {score}");
-    })
+    for a in 0..26 {
+        for i in 0..4 {
+            let frequencies = ciphertexts_frequencies.get(i).unwrap();
+            let frequency = frequencies.get(a).unwrap();
+            print!("{frequency}|");
+        }
+        println!();
+    }
 }
