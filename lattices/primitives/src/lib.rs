fa4503e72c2c030eb68b4056aa5e0468c4a1ed34
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2024-04-28 19:47:20 -0400

    Barret reduction

--- lattices/primitives/src/lib.rs
+++ lattices/primitives/src/lib.rs
@@ -2,6 +2,49 @@
 #![no_std]
 
 use core::fmt::{LowerHex, UpperHex};
+use subtle::{Choice, ConditionallySelectable};
+
+/// word size of all arithmetics
+pub type Word = u64;
+
+/// An element of the prime field Z_q where q = 3329
+#[derive(Debug, Clone, Copy)]
+pub struct FieldElem(pub Word);
+
+impl ConditionallySelectable for FieldElem {
+    fn conditional_select(a: &Self, b: &Self, choice: Choice) -> Self {
+        let val = u64::conditional_select(&a.0, &b.0, choice);
+        return Self(val);
+    }
+}
+
+impl FieldElem {
+    pub const Q: Word = 3329;
+    /// k = 2 * (floor(log2(Q)) + 1)
+    pub const BARRETT_SHIFT: usize = 24;
+    pub const BARRETT_MULTIPLIER: Word = (1 << Self::BARRETT_SHIFT) / Self::Q;
+
+    /// Assuming that the input value satisfies 0 <= val < 2*Q, perform a simple reduction
+    fn simple_reduce(val: Word) -> Self {
+        let val = Word::conditional_select(
+            &(val.wrapping_sub(Self::Q)),
+            &val,
+            Choice::from((val < Self::Q) as u8),
+        );
+
+        Self(val)
+    }
+
+    /// Perform the Barrett reduction
+    pub fn barrett_reduce(val: Word) -> Self {
+        let quot = (val * Self::BARRETT_MULTIPLIER) >> Self::BARRETT_SHIFT;
+        return Self::simple_reduce(val - quot * Self::Q);
+    }
+
+    pub fn modmul(&self, other: &Self) -> Self {
+        Self::barrett_reduce(self.0 * other.0)
+    }
+}
 
 /// A member of the polynomial ring used in Kyber:
 /// R_q = Z_q[x] / (x ** n + 1)
@@ -78,5 +121,21 @@ impl LowerHex for Poly {
         write!(f, "}}")?;
 
         return Ok(());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn field_modmul() {
+        for a in 0..FieldElem::Q {
+            for b in 0..FieldElem::Q {
+                let barret_modmul = FieldElem(a).modmul(&FieldElem(b)).0;
+                let expected = (a * b) % FieldElem::Q;
+                assert_eq!(barret_modmul, expected);
+            }
+        }
     }
 }
